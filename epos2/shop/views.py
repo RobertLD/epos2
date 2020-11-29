b34f732b8b05c089238d21d3f5eca6864265c6eb
Robert DeRienzo <robert.derienzo@Knights.ucf.edu>
2020-11-28 20:25:08 -0500

    configure admin interface and model interaction

--- epos2/shop/views.py
+++ epos2/shop/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    text_var = 'This is my first Django Project'
+    return HttpResponse(text_var)
