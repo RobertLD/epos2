c111589f305c796835e279d5c2f44b82199fec64
Robert DeRienzo <robert.derienzo@Knights.ucf.edu>
2020-12-03 20:25:13 -0500

    add test-cases

--- epos2/order/tests.py
+++ epos2/order/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from shop.models import Product, Category 
+from django.contrib.auth.models import Group, User
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        z = Category.objects.create(name="Appetizer")
+        Product.objects.create(name= "Banana", Category=z, price = 1, stock = 1, slug = "Dfdfd")
+        Product.objects.create(name= "Apple", Category=z, price = 1, stock = 1,slug = "Dfdfdsdf3e")
 
-# Create your tests here.
+    def test_product_category_creation(self):
+        lion = Product.objects.get(name="Banana")
+        self.assertEqual(lion.name, "Banana")
+        self.assertEqual(lion.Category.name, "Appetizer")
+
+    def test_product_category_search_nonexist(self):
+        try:
+            a = Product.objects.get(name="Apples")
+        except Product.DoesNotExist as e:
+            return True
+
+    def test_user_create(self):
+        try:
+            User.objects.create(username="billy", password = "ugly")
+            a = User.objects.get(username="billy")
+            if (not a.DoesNotExist):
+                return True
+            else:
+                return False   
+        except e:
+            return False
+        
+        
+
+        
